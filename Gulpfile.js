c6d1cc23528aa2f1892e2ef9e63e9b61c049fcb2
Jacob Shore <jacob@perrysysinc.com>
2019-08-18 18:42:36 +0300

    npm install

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const njr = require('gulp-nunjucks-render');
+const data = require('gulp-data');
+
+ function nunjucks(){
+    // where the nunjucks files to convert to html are 
+    return gulp.src('./pages/**/*.+(html|njk)')
+    // include the data included in data.json
+    .pipe(data(function(){
+        return require('./data.json')
+    }))
+    // actually do the conversion
+    .pipe(njr({
+        // where the template files are located
+        path: ['templates']
+    }))
+    // where to send the converted njk files => html files
+    .pipe(gulp.dest('./app/dist/'))
+}
+
+function style() {
+    // 1. where is my scss file?
+    return gulp.src('.app/src/scss/**/*.scss')
+    // 2. pass that file through sass compiler (to change to minified change outputStyle to compressed)
+    .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
+    // 3. where to save compiled css?
+    .pipe(gulp.dest('./app/dist/css'))
+    // 4. stream changes to all browsers
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    // 1. Initialize browserSync
+    browserSync.init({
+        server: {
+            // 2. Identify where the root directory of the server is
+            baseDir: './'
+        }
+    });
+
+    //  Arguments: 
+    //      1) path: where to look for changes, 
+    //      2) callback: which Gulp functions to run
+    gulp.watch('./app/src/pages/**/*.njk', nunjucks);
+    gulp.watch('./app/src/templates/**/*.njk', nunjucks);
+    gulp.watch('./app/src/scss/**/*.scss', style);
+    gulp.watch('./app/dist/*.html').on('change', browserSync.reload);
+    gulp.watch('./app/dist/js/**/*.js').on('change', browserSync.reload)
+}
+
+// these make the functions accessable from the command line by writing gulp and then the value.
+exports.watch = watch;
